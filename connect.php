7aee81da971ed9cc828ab8ddbd69f4d405e51683
Thibault-LSW <thibault.cortial@gmail.com>
2012-09-17 15:48:39 +0200

    v1
    v1

---
+++ connect.php
@@ -0,0 +1,36 @@
+<?php 
+
+session_start();
+
+//if($_SESSION['rights'] != 'null')
+//{
+//	header('Location: index.php');
+//	exit();
+//}
+
+include("include/db_connect.php");
+
+$donnees_user;
+
+// On recupere tout le contenu de la table user
+$reponse = $bdd->query('SELECT * FROM users');
+
+// On lit chaque entree une a une
+while ($donnees_user = $reponse->fetch())
+{
+	if ($donnees_user['username']== htmlspecialchars($_POST['login']) AND $donnees_user['password'] == htmlspecialchars($_POST['password']))
+	{
+		// Si le mot de passe est bon
+		// On synchronise la session avec les donnees
+		$_SESSION['user'] = $donnees_user['user'];
+		$_SESSION['rights'] = $donnees_user['rights'];
+		$reponse->closeCursor(); // Termine le traitement de la requete
+    	header('Location: home.php');
+		exit();
+	}
+}
+
+$_SESSION['VERIF']=1;
+header('Location: index.php');
+
+?>
